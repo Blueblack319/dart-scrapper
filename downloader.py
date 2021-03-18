68b49b90acfe143539cc74427785f2cd83d632e5
crazybirdz <didwogns789@yonsei.ac.kr>
2021-03-18 16:35:13 +0900

    Download 1 filepython

--- downloader.py
+++ downloader.py
@@ -20,7 +20,18 @@ options.add_experimental_option("prefs", {"download.default_directory": "/mnt/f/
 
 def open_report(name):
 
-def get_file():
+def get_file(num, driver):
+    driver.find_elements_by_xpath('//a[@title="사업보고서 공시뷰어 새창"]')[num].click()
+    popup_window = driver.window_handles[1]
+    driver.switch_to.window(f_report_window)
+    driver.find_element_by_xpath('//a[@href="#download"]').click()
+    driver.close()
+    popup_window = driver.window_handles[1]
+    driver.switch_to.window(f_download_window)
+    driver.find_element_by_xpath("//a").click()
+    driver.close()
+
+    return
 
 
 def get_files(name):
@@ -31,14 +42,9 @@ def get_files(name):
     driver.find_element_by_xpath('//a[@href="#divPublicType_01"]').click()
     driver.find_element(By.ID, "publicType1").click()
     driver.find_element_by_xpath('//input[@type="image"][@alt="검색"]').send_keys(Keys.ENTER)
-
-    driver.find_elements_by_xpath('//a[@title="사업보고서 공시뷰어 새창"]')[3].click()
-    f_report_window = driver.window_handles[1]
-    driver.switch_to.window(f_report_window)
-    driver.find_element_by_xpath('//a[@href="#download"]').click()
-    f_download_window = driver.window_handles[2]
-    driver.switch_to.window(f_download_window)
-    driver.find_element_by_xpath("//a").click()
-
+    default_window = driver.current_window_handle
+    get_file(0, driver)
+    driver.switch_to.window(default_window)
+    get_file(3, driver)
     return
 
